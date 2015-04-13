ef85716a6903693b80aa5f815de528e3c88a96e8
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 14:20:00 -0400

    Nested Method Calls

--- MethodsInObjC/main.m
+++ MethodsInObjC/main.m
@@ -7,20 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Car.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        [porsche initWithModel:@"Porsche"];
-        [porsche initWithModel:@"Porsche" mileage:42000.0];
+        //[porsche initWithModel:@"Porsche"];
+        //[porsche initWithModel:@"Porsche" mileage:42000.0];
         
         // Python/Java/C++
-        porsche.drive("Home", "Airport");
+        //porsche.drive("Home", "Airport");
         
         // Objective-C
-        [porsche driveFromOrigin:@"Home" toDestination:@"Airport"];
+        //[porsche driveFromOrigin:@"Home" toDestination:@"Airport"];
         
+        // JavaScript
+        Car.alloc().init();
         
+        // Objective-C
+        [[Car alloc] init];
         
     }
     return 0;
